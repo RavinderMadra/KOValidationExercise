c0794a10bb8a897ac4d65c46858187b68449dd91
ravinderkmadra <ravinderkmadra@gmail.com>
2019-02-16 07:30:28 +1300

    initial commit

---
+++ KnockoutValidationExmp/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using KnockoutValidationExmp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnockoutValidationExmp.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseFirstDbEntities _db = new DatabaseFirstDbEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Create(tblCustomerRec obj)
+        {
+            
+            if(obj.Id!=0){
+               var res= _db.tblCustomerRecs.Where(x=>x.Id==obj.Id).FirstOrDefault();
+                if(res!=null)
+                {
+                    res.Name = obj.Name;
+                    res.Address = obj.Address;
+                    _db.SaveChanges();
+                } 
+            }
+            else
+            {
+                _db.tblCustomerRecs.Add(obj);
+                _db.SaveChanges();
+            }
+            
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult ListCustomer()
+        {
+            var list = _db.tblCustomerRecs.ToList();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+        
+        [HttpPost]
+        public ActionResult Delete(long Id)
+        {
+            var getRec = _db.tblCustomerRecs.Find(Id);
+               _db.tblCustomerRecs.Remove(getRec);
+               _db.SaveChanges();
+            return ListCustomer();
+        }
+        [HttpGet]
+         public JsonResult Edit(int Id)
+        {
+            var getrec= _db.tblCustomerRecs.Find(Id);
+            return Json(getrec, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ KnockoutValidationExmp/Scripts/KoValidationFile.js
@@ -0,0 +1,151 @@
+﻿
+$(function () {
+    
+    ko.validation.init({
+        registerExtenders: true,
+        messagesOnModified: true,
+        insertMessages: true,
+        parseInputAttributes: true,
+        errorClass: 'errorStyle',
+        messageTemplate: null
+
+    }, true);
+    createmodel.list();
+
+
+
+var createmodel = {
+
+    Id: ko.observable(0),
+    Name: ko.observable().extend({ required: true, minLength: 2, maxLength: 17 }),
+    Address: ko.observable().extend({ required: true, minLength: 2, maxLength: 17 }),
+    Customerrec: ko.observableArray([]),
+
+    list: function () {
+        var thisObj = this;
+        $.ajax({
+            url: '/Home/ListCustomer',
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+
+                thisObj.Customerrec(data); //Here we are assigning values to KO Observable array
+
+            },
+            error: function () {
+                alert('Record not fetch');
+            }
+        });
+
+    },
+
+
+    submit: function () {
+        debugger;
+        if (createmodel.errors().length === 0) {
+            debugger;
+            $.ajax({
+
+                url: '/Home/Create',
+                type: 'post',
+                dataType: 'json',
+                data: ko.toJSON(this), //Here the data wil be converted to JSON
+                contentType: 'application/json',
+                success: function (db) {
+                    clearTextBox();
+                    $('#myModal').modal('hide');
+                    alert('data saved');                                      
+                  
+                    createmodel.list(); 
+                    
+                },
+                error: function (db) {
+
+                    alert('data not saved');
+                }
+            });
+        }
+
+        else {
+            debugger;
+            alert('Please check your submission');
+
+        }
+
+    },
+
+
+    Edit: function (Id) {
+        debugger;
+        $.ajax({
+            url: "/Home/Edit/?Id=" + Id,
+            type: "GET",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (res) {
+                debugger;
+                $('#myModal').modal({
+                    show: 'true'
+                });
+                createmodel.Id(res.Id);
+                createmodel.Name(res.Name);
+                createmodel.Address(res.Address);
+            },
+            error: function () {
+                alert("data not edited");
+            }
+        });
+
+    },
+
+    Delete: function (Id) {
+        debugger;
+        var ans = confirm("Are you sure you want to delete this Record?");
+        if (ans) {
+            $.ajax({
+                url: "/Home/Delete/?Id=" + Id,
+                type: "POST",
+                contentType: "application/json;charset=UTF-8",
+                dataType: "json",
+                success: function (res) {
+                    createmodel.list();
+                    alert("data deleted");
+                },
+                error: function () {
+                    alert("data not deleted");
+                }
+            });
+        }
+
+        else {
+            debugger;
+            alert('error in remove data');
+        }
+
+    },
+
+
+};
+
+function Customerrec(data) {
+
+    this.Id = ko.observable(data.Id);
+    this.Name = ko.observable(data.Name);
+    this.Address = ko.observable(data.Address);
+}
+
+function clearTextBox() {
+
+    $('#Name').val("");
+    $('#Add').val("");
+    $('#btnUpdate').hide();
+    $('#btnAdd').show();
+    createmodel.Id = 0;
+    createmodel.Name = "";
+    createmodel.Address = "";
+}
+createmodel.errors = ko.validation.group(createmodel);
+    ko.applyBindings(createmodel);
+
+});
